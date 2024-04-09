de0e4b3aa4abcfbd27e1d8460235f5dfb77b8d4b
Kipkiruie <elishak551@gmail.com>
2024-04-08 19:01:14 -0700

    configuration

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,11 @@
-exec { 'pkill killmenow':
-  path => '/usr/bin:/usr/sbin:/bin'
+# 2-execute_a_command.pp
+
+exec { 'kill':
+  command     => 'pkill -f killmenow',
+  refreshonly => true, # This ensures the command is only executed if a resource notifies it.
+}
+
+notify { 'Command executed successfully':
+  subscribe => Exec['kill'], # This notifies the 'notify' resource when the 'exec' resource is executed successfully.
 }
 
