8baed30398b00f3c8c200f54d25b43c4224a2e08
Kipkiruie <elishak551@gmail.com>
2024-04-23 10:23:15 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,28 +4,37 @@
 import requests
 import sys
 
+def get_employee_info(employee_id):
+    try:
+        base_url = "https://jsonplaceholder.typicode.com/users"
+        employee_url = f"{base_url}/{employee_id}"
+
+        response = requests.get(employee_url)
+        response.raise_for_status()  # Raise an exception for bad response
+        employee_data = response.json()
+        employee_name = employee_data.get('name')
+
+        todo_url = f"{employee_url}/todos"
+        response = requests.get(todo_url)
+        response.raise_for_status()  # Raise an exception for bad response
+        tasks = response.json()
+
+        done_tasks = [task for task in tasks if task.get('completed')]
+        done = len(done_tasks)
+        total_tasks = len(tasks)
+
+        print(f"Employee {employee_name} is done with tasks({done}/{total_tasks}):")
+        for task in done_tasks:
+            print(f"\t{task.get('title')}")
+
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-    response = requests.get(url)
-    employeeName = response.json().get('name')
+    employee_id = sys.argv[1]
+    get_employee_info(employee_id)
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
-
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
